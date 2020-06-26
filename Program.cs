e650a500a053b264ed3e403c648e573dc1b8603c
Your Name <capintheshadow@gmail.com>
2020-06-26 12:51:27 +0300

    program.cs changing

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@ namespace YoTeztn
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
